b662a6e15878c82941163da132739dc388443977
Nilesh Suthar <nil.15111993@gmail.com>
2014-09-13 23:19:57 +0530

    Nows sends nums to client

--- index.js
+++ index.js
@@ -2,28 +2,40 @@ var http = require("http");
 var Primus = require("primus");
 var connect = require('connect')
 var createStatic = require('connect-static');
-
+var _ = require('lodash');
 var app = connect();
+var nums = _.range(1,101)
+var num = _.range(1,101)
 createStatic(options, function(err, middleware) {
   if (err) throw err;
   app.use('/', middleware);
 });
-
 var server = http.createServer(app);
 var options = {
   transformer: "engine.io"
 }
 var primus = new Primus(server, options);
-
 primus.on('connection', function(socket) {
   console.log("client has connected");
-  socket.write("Hello Client, I am Server");
   socket.on('data', function(message) {
     console.log('recieved a new message', message);
-    socket.write({
-      ping: 'pong'
-    });
+    if(message.msg=='list'){
+      nums = _.shuffle(nums);
+      socket.write( {"msg":"list","nums": _.first(nums,25) });
+    }
   });
 });
+
+game = setInterval(sendNewnum,50);
+function sendNewnum(){
+  num = _.shuffle(num);
+  n = num.splice(0,1);
+  console.log(n);
+  primus.write(n);
+  if(num.length==0){
+    console.log('Game Over');
+    clearInterval(game)
+  }
+}
 
 server.listen(3000);
